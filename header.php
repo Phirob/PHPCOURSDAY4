29f2fd981432651a8a8e0ed476b90e5a91d97716
Phirob <49711037+Phirob@users.noreply.github.com>
2020-02-25 15:59:55 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Page de connexion</title>
+    <link rel='stylesheet' href="style.css">
+</head>
+<body>
+    <ul class="nav">
+        <li> <a href="accueil.php">Accueil</a> </li>
+        <li> <a href="connexion.php">Connexion</a> </li>
+        <li> <a href="inscription.php">Inscription</a> </li>
+    </ul>
